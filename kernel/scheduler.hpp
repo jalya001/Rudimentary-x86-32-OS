a073d6b02c4637c7696f65c45dcf2f87f8f441ef
anisai <anisai@uio.no>
2026-08-18 00:41:05 +0200

    scheduler: give exit() C linkage so entry.S can call it

--- kernel/scheduler.hpp
+++ kernel/scheduler.hpp
@@ -5,7 +5,8 @@
 void yield(void);
 void scheduler(void);
 void dispatch(void);
-void exit(void);
+
+extern "C" void exit(void);
 
 void init_kernel_thread_stack(tcb_t *t, void (*entry_fn)());
 void init_user_thread_stack(tcb_t *t, void (*entry_fn)(), uintptr_t user_stack);
